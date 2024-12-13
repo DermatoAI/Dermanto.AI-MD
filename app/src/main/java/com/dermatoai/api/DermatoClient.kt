0f898442dd3af3ba44ec2dc71ef34f0aa3179939
muammarzaki <muammarzaki2004@gmail.com>
2024-12-13 08:17:23 +0700

    set timeout more longer

--- app/src/main/java/com/dermatoai/api/DermatoClient.kt
+++ app/src/main/java/com/dermatoai/api/DermatoClient.kt
@@ -7,6 +7,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 
 /**
@@ -20,6 +21,8 @@ object DermatoClient {
 
 
     private val okHttpClient = OkHttpClient.Builder()
+        .readTimeout(60, TimeUnit.SECONDS)
+        .connectTimeout(120, TimeUnit.SECONDS)
         .addInterceptor(HttpLoggingInterceptor().apply {
             level = HttpLoggingInterceptor.Level.BODY
         })
